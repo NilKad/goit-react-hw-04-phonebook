0c25e0c936c7a30c2be1c8c7e107f9a953faa888
NilKad <kadulin@gmail.com>
2022-11-15 00:33:09 +0200

    v1

--- src/components/Phonebook/Phonebook.jsx
+++ src/components/Phonebook/Phonebook.jsx
@@ -5,12 +5,12 @@ import { Filter } from '../Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 
 export default function Phonebook() {
-  const defaultContacts = [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-    { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-  ];
+  // const defaultContacts = [
+  //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  // ];
 
   const [contacts, setContacts] = useState([]);
   const [filter, setFilter] = useState('');
@@ -22,7 +22,12 @@ export default function Phonebook() {
     if (readContacts) {
       setContacts(readContacts);
     } else {
-      setContacts(defaultContacts);
+      setContacts([
+        { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+        { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+        { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+        { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      ]);
     }
   }, []);
 
